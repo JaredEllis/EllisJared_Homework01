d8ffc3146e4dae8ca615ac3dd2ece304a60dde19
Jared <2008ellis@gmail.com>
2023-02-08 22:39:39 -0600

    power up base + rapid fire power up

--- Assets/InheritanceExample/Scripts/Enemies/Crawler.cs
+++ Assets/InheritanceExample/Scripts/Enemies/Crawler.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Crawler : EnemyBase
 {
+    [SerializeField]
+    private GameObject RapidFirePowerUp;
+    
     protected override void OnHit()
     {
         
@@ -13,7 +16,7 @@ public class Crawler : EnemyBase
     public override void Kill()
     {
         //TODO put code you want to happen before disable here
-
+        Instantiate(RapidFirePowerUp, transform.position, Quaternion.identity);
         // this runs the base method AND what's above it here
         base.Kill();
     }
